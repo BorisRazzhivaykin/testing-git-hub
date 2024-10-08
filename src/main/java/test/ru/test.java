0fd0e260c116525577f614d52f1b08d727d481b2
Boris Razzhivaykin <rboris93@mail.ru>
2024-10-08 14:55:01 +0300

    добавил свое имя

--- src/main/java/test/ru/test.java
+++ src/main/java/test/ru/test.java
@@ -2,6 +2,6 @@ package test.ru;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("Hello World, my name is Boris");
     }
 }
